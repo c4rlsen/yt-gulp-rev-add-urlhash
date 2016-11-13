a37e60b70e480f712840335919e58e7007c60959
rodsonluo <rodsonluo@gmail.com>
2016-11-13 12:25:43 +0800

    fix bug again

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ function plugin(options) {
           }
 
           // replace absolute path
+          contents = contents.split(unreved).join(reved);
           contents = contents.split(revedOri).join(reved);
 
           if (options.prefix) {
