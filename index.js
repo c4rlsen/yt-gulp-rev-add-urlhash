aa8afa73f04403daa10c4b6388733282dc7638be
rodsonluo <rodsonluo@gmail.com>
2016-11-13 12:30:27 +0800

    fix bug again and again

--- index.js
+++ index.js
@@ -99,8 +99,11 @@ function plugin(options) {
           }
 
           // replace absolute path
-          contents = contents.split(unreved).join(reved);
-          contents = contents.split(revedOri).join(reved);
+          if (contents.indexOf(unreved) > 0) {
+            contents = contents.split(unreved).join(reved);
+          } else {
+            contents = contents.split(revedOri).join(reved);
+          }
 
           if (options.prefix) {
             contents = contents.split('/' + options.prefix).join(options.prefix + '/');
